b44133bd89ce78c963a15c2a256329fde557bb00
balaji-githubstore <31404824+balaji-githubstore@users.noreply.github.com>
2021-09-23 17:59:57 +0530

    demo3-excel to array

---
+++ Demo3Test.java
@@ -0,0 +1,43 @@
+package com.voya.test;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+
+public class Demo3Test {
+
+	public static void main(String[] args) throws IOException   {
+
+		FileInputStream file = new FileInputStream("testdata/OpenEMRTestData.xlsx"); // location 
+
+		XSSFWorkbook book = new XSSFWorkbook(file); // open
+
+		XSSFSheet sheet = book.getSheet("invalidCredentialTest"); //sheet
+		
+		String[][] main=new String[2][4];
+		
+		for(int r=1;r<3;r++)
+		{
+			for(int c=0;c<4;c++)
+			{
+				XSSFCell cell =  sheet.getRow(r).getCell(c);  //row //cell
+				
+				DataFormatter format=new DataFormatter();
+				String value = format.formatCellValue(cell);
+				System.out.println(value);
+				main[r-1][c]=value ;
+			}
+		}
+		
+		System.out.println("end---");
+	}
+}
+
+
+
+
