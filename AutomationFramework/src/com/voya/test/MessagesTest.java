59ba0798e868c55bd249eb2b95b3634e760d9bd1
balaji-githubstore <31404824+balaji-githubstore@users.noreply.github.com>
2021-09-23 19:54:46 +0530

    updated dataprovider

--- AutomationFramework/src/com/voya/test/MessagesTest.java
+++ AutomationFramework/src/com/voya/test/MessagesTest.java
@@ -14,6 +14,7 @@ import org.testng.annotations.Test;
 import com.voya.pages.DashboardPage;
 import com.voya.pages.LoginPage;
 import com.voya.pages.MessageCenterPage;
+import com.voya.utilities.DataProviderUtils;
 
 public class MessagesTest {
 	
@@ -36,29 +37,31 @@ public class MessagesTest {
 		driver.quit();
 	}
 	
-	@Test
-	public void patientErrorMessageTest()
+	@Test(dataProviderClass = DataProviderUtils.class,dataProvider = "patientErrorMessageData")
+	public void patientErrorMessageTest(String username,String password,String language,String message,String type,String user,String expectedValue)
 	{
-		LoginPage.enterUsername(driver, "admin");
-		LoginPage.enterPassword(driver, "pass");
-		LoginPage.selectLanguage(driver, "English (Indian)");
+		LoginPage.enterUsername(driver, username);
+		LoginPage.enterPassword(driver, password);
+		LoginPage.selectLanguage(driver, language);
 		LoginPage.clickOnLogin(driver);
 		
 		DashboardPage.clickOnMessages(driver);
 		
 		MessageCenterPage.switchToMsgFrame(driver);
 		MessageCenterPage.clickOnAddNew(driver);
-		MessageCenterPage.typeMessage(driver, "please book an appointment.");
-		MessageCenterPage.selectType(driver, "Pharmacy");
-		MessageCenterPage.selectUserByIndex(driver, 1);
+		MessageCenterPage.typeMessage(driver, message);
+		MessageCenterPage.selectType(driver, type);
+		
+		int index=Integer.parseInt(user);
+		MessageCenterPage.selectUserByIndex(driver, index);
+		
 		MessageCenterPage.clickOnSendMessage(driver);
 		String actualText=MessageCenterPage.getPatientErrorMessage(driver);
 		MessageCenterPage.switchOutOfFrame(driver);
 		
-		Assert.assertEquals(actualText, "Please choose a patient");
+		Assert.assertEquals(actualText, expectedValue);
 		
 	}
-
 }
 
 
