b014adc6a185e82da545cdf34bb8567d73abd57e
balaji-githubstore <31404824+balaji-githubstore@users.noreply.github.com>
2021-09-24 21:17:24 +0530

    automation framework - final

--- AutomationFramework/src/com/voya/pages/LoginPage.java
+++ AutomationFramework/src/com/voya/pages/LoginPage.java
@@ -7,43 +7,51 @@ import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class LoginPage {
+	
+	private static By usernameLocator=By.name("authUser");
+	private static By passwordLocator=By.id("clearPass");
+	private static By languageLocator=By.name("languageChoice");
+	private static By errorLocator=By.xpath("//button[@type='submit']");
+	private static By invalidLocator=By.xpath("//div[contains(text(),'Invalid')]");	
+	private static By descLocator=By.xpath("//p[contains(text(),'most')]");
+	
 
 	public static void enterUsername(WebDriver driver, String username) 
 	{
-		driver.findElement(By.name("authUser")).sendKeys(username);
+		driver.findElement(usernameLocator).sendKeys(username);
 	}
 
 	public static void enterPassword(WebDriver driver, String password) 
 	{
-		driver.findElement(By.id("clearPass")).sendKeys(password);
+		driver.findElement(passwordLocator).sendKeys(password);
 	}
 	
 	public static void selectLanguage(WebDriver driver,String languageText)
 	{
-		Select selectLanguage=new Select(driver.findElement(By.name("languageChoice")));
+		Select selectLanguage=new Select(driver.findElement(languageLocator));
 		selectLanguage.selectByVisibleText(languageText);
 	}
 	
 	public static void clickOnLogin(WebDriver driver)
 	{
-		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		driver.findElement(errorLocator).click();
 	}
 	
 	public static String getInvalidErrorMessage(WebDriver driver)
 	{
-		String text= driver.findElement(By.xpath("//div[contains(text(),'Invalid')]")).getText();
+		String text= driver.findElement(invalidLocator).getText();
 		return text;
 	}
 	
 	public static String getApplicationDescription(WebDriver driver)
 	{
-		return driver.findElement(By.xpath("//p[contains(text(),'most')]")).getText();
+		return driver.findElement(descLocator).getText();
 	}
 	
 	public static void waitForPresenceOfUsernameElement(WebDriver driver)
 	{
 		WebDriverWait wait=new WebDriverWait(driver, 50);
-		wait.until(ExpectedConditions.presenceOfElementLocated(By.id("authUser")));
+		wait.until(ExpectedConditions.presenceOfElementLocated(usernameLocator));
 	}
 	
 	public static String getLoginPageTitle(WebDriver driver)
